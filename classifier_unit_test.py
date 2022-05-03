545fb3dcbe046255625a7d6d8a4f0a6bd08d501d
kroglkt <lkt259@alumni.ku.dk>
2022-05-03 09:32:31 +0200

    aaaa please work

--- classifier_unit_test.py
+++ classifier_unit_test.py
@@ -8,6 +8,11 @@ from matplotlib import pyplot as plt
 
 def permutate(clf, X_train, y_train):
     print("Perfoming permutation importance...")
+    if type(X_train) != np.ndarray:
+        X_train = X_train.toarray()
+    if type(y_train) != np.ndarray:
+        y_train = y_train.toarray()
+
     result = permutation_importance(clf, X_train, y_train)
     print("Permutation imporance done! Note, that vector features are not accounted for. Raw importances returned.")
     return result
@@ -30,7 +35,11 @@ def fpr_tpr(classifier, X_train, X_test, y_train, y_test):
     clf = OneVsRestClassifier(classifier)
 
     y_score = clf.fit(X_train, y_train).predict_proba(X_test)
-    fpr, tpr, _ = roc_curve(y_test.ravel(), y_score.ravel())
+    print(y_test.ravel().shape, y_score.ravel().shape)
+    if y_test.ravel().shape == y_score.ravel().shape:
+        fpr, tpr, _ = roc_curve(y_test.ravel(), y_score.ravel())
+    else:
+        fpr, tpr, _ = roc_curve(y_test.ravel(), y_score[:,0])
 
     return fpr, tpr
 
@@ -67,7 +76,7 @@ def test_classifier(classifier, X_train, X_test, y_train, y_test, strategy="most
     f1 = f1_score(y_test, preds, average='weighted')
     dum_f1 = f1_score(y_test, dum_preds, average='weighted')
     
-    if len(set(y_test)==2):
+    if len(set(y_test))==2:
         auc = roc_auc_score(y_test, preds_prob[:,1])
         dum_auc = roc_auc_score(y_test, dum_probs[:,1])
     else:
